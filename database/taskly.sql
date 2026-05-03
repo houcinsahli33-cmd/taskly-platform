866f0d2f6ac6df49e0d8287589627c4e13744113
Hocine <houcinsahli33@gmail.com>
2026-05-03 20:30:27 +0200

    Add database folder

---
+++ database/taskly.sql
@@ -0,0 +1,49 @@
+CREATE database  IF NOT EXISTS taskly_db;
+USE taskly_db;
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL UNIQUE,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    role ENUM('client', 'artisant', 'admin') NOT NULL ,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table des services. Elle contient les catégories comme plomberie, électricité, peinture...
+CREATE TABLE IF NOT EXISTS services (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    description TEXT,
+    image VARCHAR(255)
+);
+
+-- Table des artisans. Elle contient les informations spécifiques aux artisans
+CREATE TABLE IF NOT EXISTS artisans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    service_id INT NOT NULL,
+    ville VARCHAR(255) NOT NULL,
+    telephone VARCHAR(20),
+    description TEXT,
+    experience INT DEFAULT 0,
+    photo VARCHAR(255),
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, 
+    FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE
+);
+
+-- Table des demandes envoyées par les clients aux artisans
+CREATE TABLE IF NOT EXISTS demandes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id INT NOT NULL,
+    artisan_id INT NOT NULL,
+    message TEXT,
+    adresse VARCHAR(255),
+    date_souhaitee DATE,
+    statut ENUM('en_attente', 'acceptee', 'refusee') DEFAULT 'en_attente',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (client_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (artisan_id) REFERENCES artisans(id) ON DELETE CASCADE
+);
